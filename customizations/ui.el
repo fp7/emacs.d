2ccaa57e4ea6f6a1fcb7055d4cd8641322dc5c25
Finn Petersen <4peterse@googlemail.com>
2016-01-10 20:35:49 +0100

    Fullheight and 100 wide
    Change-Id: I57c4e6d4616199f93616e18a46cfa924e5038248

--- customizations/ui.el
+++ customizations/ui.el
@@ -8,7 +8,7 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
-(setq initial-frame-alist '((fullscreen . fullwidth) (heigth. 1000) (top . 0) (left . 0)))
+(setq initial-frame-alist '((width . 100) (fullscreen . fullheight) (top . 0) (left . 0)))
 
 (setq mouse-yank-at-point t)
 (blink-cursor-mode 0)
