667fe1f3a863569a94ea1bdd45bbba79a9934d58
Finn Petersen <4peterse@googlemail.com>
2016-02-07 17:41:16 +0100

    Update clj-refactor, clojure-mode, auctex
    Reenabling clj-refactor

--- customizations/clojure.el
+++ customizations/clojure.el
@@ -19,11 +19,11 @@
 (setq cider-repl-display-in-current-window t)
 
 
-;; (require 'clj-refactor)
+(require 'clj-refactor)
 
-;; (defun my-clojure-mode-hook ()
-;;     (clj-refactor-mode 1)
-;;     (yas-minor-mode 1) ; for adding require/use/import
-;;     (cljr-add-keybindings-with-prefix "C-c C-m"))
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
 
-;; (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
